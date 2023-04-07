16171a799b04b0425b6c944271685dcbac2bf5bf
Oliver Scotten <revi99@me.com>
2023-04-07 13:26:25 +0000

    Added /delete/report/format

--- app/main.py
+++ app/main.py
@@ -532,6 +532,25 @@ async def delete_report(
             gmp.authenticate(username=current_user.username, password=PASSWORD)
         return Response(content=gmp.delete_report(report_id=report_id), media_type="application/xml")
 
+@app.post("/delete/report/format", tags=["report"])
+async def delete_report(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+    report_format_id: Union[str, ReportFormatType]
+):
+    """Clone a report format from an existing one
+
+        Arguments:
+
+            report_format_id: UUID of the existing report format or ReportFormatType (enum)
+
+        Returns:
+            The response.
+        """
+    with Gmp(connection=CONNECTION) as gmp:
+        if verify_password(PASSWORD, current_user.hashed_password):
+            gmp.authenticate(username=current_user.username, password=PASSWORD)
+        return Response(content=gmp.delete_report_format(report_format_id=report_format_id), media_type="application/xml")
+
 ### USER DATA ###
 
 @app.get("/get/user", tags=["user"])
