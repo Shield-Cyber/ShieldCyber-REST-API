3b0bec242d094d5679958d30b54a32477a1ebb6d
Oliver Scotten <revi99@me.com>
2023-04-24 14:58:50 +0000

    Minor: Added /get/port/lists

--- app/main.py
+++ app/main.py
@@ -916,3 +916,29 @@ async def get_scanners(
         if Auth.verify_password(PASSWORD, current_user.hashed_password):
             gmp.authenticate(username=current_user.username, password=PASSWORD)
         return Response(content=gmp.get_scanners(filter_string=filter_string,filter_id=filter_id,trash=trash,details=details), media_type="application/xml")
+
+@app.get("/get/port/lists", tags=["port"])
+async def get_port_lists(
+    current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)],
+    filter_string: Optional[str] = None,
+    filter_id: Optional[str] = None,
+    details: Optional[bool] = None,
+    targets: Optional[bool] = None,
+    trash: Optional[bool] = None,
+    ):
+    """Request a list of port lists
+
+        Arguments:
+            filter_string: Filter term to use for the query
+            filter_id: UUID of an existing filter to use for the query
+            details: Whether to include full port list details
+            targets: Whether to include targets using this port list
+            trash: Whether to get port lists in the trashcan instead
+
+        Returns:
+            The response.
+        """
+    with Gmp(connection=CONNECTION) as gmp:
+        if Auth.verify_password(PASSWORD, current_user.hashed_password):
+            gmp.authenticate(username=current_user.username, password=PASSWORD)
+        return Response(content=gmp.get_port_lists(filter_string=filter_string,filter_id=filter_id,trash=trash,details=details,targets=targets), media_type="application/xml")
