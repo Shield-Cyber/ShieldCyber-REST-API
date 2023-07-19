a72eedd2dc9cc71671c89e8705f13ad57b1b8365
Oliver Scotten <revi99@me.com>
2023-07-19 15:24:46 +0000

    Updated Scanner Routes

--- app/routes/scanner/models.py
+++ app/routes/scanner/models.py
@@ -1,1 +1,23 @@
 from dataclasses import dataclass
+from typing import Optional
+from gvm.protocols.gmpv208.entities.scanners import ScannerType
+
+@dataclass
+class CreateScanner:
+    name: str
+    host: str
+    port: int
+    scanner_type: ScannerType
+    credential_id: str
+    ca_pub: Optional[str] = None
+    comment: Optional[str] = None
+
+@dataclass
+class ModifyScanner:
+    scanner_type: Optional[ScannerType] = None
+    host: Optional[str] = None
+    port: Optional[int] = None
+    comment: Optional[str] = None
+    name: Optional[str] = None
+    ca_pub: Optional[str] = None
+    credential_id: Optional[str] = None
--- app/routes/scanner/scanner.py
+++ app/routes/scanner/scanner.py
@@ -8,6 +8,8 @@ from gvm.connections import UnixSocketConnection
 from typing import Annotated, Optional
 from app import LOGGING_PREFIX
 
+from . import models as Models
+
 ENDPOINT = "scanner"
 
 LOGGER = logging.getLogger(f"{LOGGING_PREFIX}.{ENDPOINT}")
@@ -44,5 +46,146 @@ async def get_scanners(
         gmp.authenticate(username=current_user.username, password=Auth.get_admin_password())
         try:
             return gmp.get_scanners(filter_string=filter_string,filter_id=filter_id,trash=trash,details=details)
+        except Exception as err:
+            return ErrorResponse(err)
+
+@ROUTER.get("/get/{scanner_id}")
+async def get_scanner(
+    current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)],
+    scanner_id: str
+    ):
+    """Request a single scanner
+
+        Arguments:
+
+            scanner_id: UUID of an existing scanner
+
+        Returns:
+            The response.
+        """
+    with Gmp(connection=UnixSocketConnection()) as gmp:
+        gmp.authenticate(username=current_user.username, password=Auth.get_admin_password())
+        try:
+            return gmp.get_scanner(scanner_id=scanner_id)
+        except Exception as err:
+            return ErrorResponse(err)
+        
+@ROUTER.post("/create")
+async def create_scanner(
+    current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)],
+    Base: Models.CreateScanner
+    ):
+    """Create a new scanner
+
+        Arguments:
+
+            name: Name of the scanner
+            host: The host of the scanner
+            port: The port of the scanner
+            scanner_type: Type of the scanner.
+            credential_id: UUID of client certificate credential for the scanner
+            ca_pub: Certificate of CA to verify scanner certificate
+            comment: Comment for the scanner
+
+        Returns:
+            The response.
+        """
+    with Gmp(connection=UnixSocketConnection()) as gmp:
+        gmp.authenticate(username=current_user.username, password=Auth.get_admin_password())
+        try:
+            return gmp.create_scanner(name=Base.name,host=Base.host,port=Base.port,scanner_type=Base.scanner_type,credential_id=Base.credential_id,ca_pub=Base.ca_pub,comment=Base.comment)
+        except Exception as err:
+            return ErrorResponse(err)
+        
+@ROUTER.patch("/modify/{scanner_id}")
+async def modify_scanner(
+    current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)],
+    scanner_id: str,
+    Base: Models.ModifyScanner
+    ):
+    """Modifies an existing scanner.
+
+        Arguments:
+
+            scanner_id: UUID of scanner to modify.
+            scanner_type: New type of the Scanner.
+            host: Host of the scanner.
+            port: Port of the scanner.
+            comment: Comment on scanner.
+            name: Name of scanner.
+            ca_pub: Certificate of CA to verify scanner's certificate.
+            credential_id: UUID of the client certificate credential for the Scanner.
+
+        Returns:
+            The response.
+        """
+    with Gmp(connection=UnixSocketConnection()) as gmp:
+        gmp.authenticate(username=current_user.username, password=Auth.get_admin_password())
+        try:
+            return gmp.modify_scanner(scanner_id=scanner_id,scanner_type=Base.scanner_type,host=Base.host,port=Base.port,comment=Base.comment,name=Base.name,ca_pub=Base.ca_pub,credential_id=Base.credential_id)
+        except Exception as err:
+            return ErrorResponse(err)
+        
+@ROUTER.post("/clone/{scanner_id}")
+async def clone_scanner(
+    current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)],
+    scanner_id: str
+    ):
+    """Clone an existing scanner
+
+        Arguments:
+
+            scanner_id: UUID of an existing scanner to clone from
+
+        Returns:
+            The response.
+        """
+    with Gmp(connection=UnixSocketConnection()) as gmp:
+        gmp.authenticate(username=current_user.username, password=Auth.get_admin_password())
+        try:
+            return gmp.clone_scanner(scanner_id=scanner_id)
+        except Exception as err:
+            return ErrorResponse(err)
+        
+@ROUTER.delete("/delete/{scanner_id}")
+async def delete_scanner(
+    current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)],
+    scanner_id: str,
+    ultimate: Optional[bool] = False
+    ):
+    """Deletes an existing scanner
+
+        Arguments:
+
+            scanner_id: UUID of the scanner to be deleted.
+            ultimate: Whether to remove entirely, or to the trashcan.
+        """
+    with Gmp(connection=UnixSocketConnection()) as gmp:
+        gmp.authenticate(username=current_user.username, password=Auth.get_admin_password())
+        try:
+            return gmp.delete_scanner(scanner_id=scanner_id,ultimate=ultimate)
+        except Exception as err:
+            return ErrorResponse(err)
+        
+@ROUTER.get("/verify/{scanner_id}")
+async def verify_scanner(
+    current_user: Annotated[Auth.User, Depends(Auth.get_current_active_user)],
+    scanner_id: str
+    ):
+    """Verify an existing scanner
+
+        Verifies if it is possible to connect to an existing scanner. It is *not* verified if the scanner works as expected by the user.
+
+        Arguments:
+
+            scanner_id: UUID of the scanner to be verified
+
+        Returns:
+            The response.
+        """
+    with Gmp(connection=UnixSocketConnection()) as gmp:
+        gmp.authenticate(username=current_user.username, password=Auth.get_admin_password())
+        try:
+            return gmp.verify_scanner(scanner_id=scanner_id)
         except Exception as err:
             return ErrorResponse(err)
